56d78698513d0d915595239f271f03688ec4147a
Álvaro Faúndez <alvaro@faundez.net>
2017-08-06 21:14:34 -0400

    Add source and issues url to metadata

--- metadata.rb
+++ metadata.rb
@@ -9,14 +9,6 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 
 supports 'ubuntu'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/nodenv/issues'
+issues_url 'https://github.com/afaundez/nodenv-cookbook/issues'
 
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/nodenv'
+source_url 'https://github.com/afaundez/nodenv-cookbook'
