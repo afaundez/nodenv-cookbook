f221e7f8b8143e023dd8f5d9a0b5ce6ad81c85f9
Alvaro Faundez <alvaro@faundez.net>
2018-05-04 21:57:46 -0400

    Bump to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alvaro@faundez.net'
 license 'MIT'
 description 'Installs/Configures nodenv'
 long_description 'Installs/Configures nodenv'
-version '1.0.0'
+version '1.0.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/afaundez/nodenv-cookbook/issues'
