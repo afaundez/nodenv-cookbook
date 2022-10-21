a98d951629f68d615b9e24dca0a9041ea8fee957
HP41 <hiteshprab@gmail.com>
2022-10-21 13:04:29 -0400

    Fix deprecated property `user` reference

--- resources/user.rb
+++ resources/user.rb
@@ -28,8 +28,8 @@ action :install do
   end
 
   node_build_plugin_install ::File.join(nodenv_plugins_path, 'node-build') do
-    owner new_resource.user
-    group new_resource.user
+    owner new_resource.owner
+    group new_resource.owner
   end
 end
 
